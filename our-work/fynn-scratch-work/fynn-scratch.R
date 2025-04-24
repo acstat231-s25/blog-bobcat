12a5781c015f6a289956aa62a59c549b10074a9d
FynnrhrAmherst28 <fhaytonruffner25@amherst.edu>
2025-04-24 14:47:32 -0400

    added bigram plots for tfidf

--- our-work/fynn-scratch-work/fynn-scratch.R
+++ our-work/fynn-scratch-work/fynn-scratch.R
@@ -91,10 +91,11 @@ stop_words <- bind_rows(
 )
 
 # ===============================================================================
-# WHAT ARE THE MOST IMPORTANT WORDS TO EACH SUBREDDIT? -TF-IDF
+# WHAT ARE THE MOST IMPORTANT WORDS/BIGRAMS TO EACH SUBREDDIT? -TF-IDF
 # ===============================================================================
 
-word_freq_by_subr  <- all_posts |>
+# token frequencies
+word_freqs  <- all_posts |>
   # get all tokens from the content
   unnest_tokens(output = word, input = content) |>
   # remove stop words
@@ -105,14 +106,30 @@ word_freq_by_subr  <- all_posts |>
   count(word)
 
 bigram_freqs <- all_posts |>
-  unnest_tokens(output = bigram, input = content, ngram=2) |>
-  
+  unnest_tokens(output = bigram, input = content, token="ngrams", n=2) |>
+  separate(bigram, into = c("w1","w2"), sep = " ") |>
+  filter(!w1 %in% stop_words$word,
+         !w2 %in% stop_words$word) |>
+  # 3. Re-unite into a single bigram string
+  unite(bigram, w1, w2, sep = " ") |>
+  count(subreddit, bigram, sort = TRUE)
 
-subr_tfidf <- word_freq_by_subr |>
+# tfidf calculation
+word_tfidf <- word_freqs |>
   # gets tf, idf, and tf-idf all in one
   bind_tf_idf(term = word, document = subreddit, n = n) 
 
-subr_top10_tfidf <- subr_tfidf |>
+bigram_tfidf <- bigram_freqs |>
+  bind_tf_idf(term = bigram, document = subreddit, n = n)
+
+top_word_tfidf <- word_tfidf |>
+  # arrange in descending order to get highest tf-dfs 
+  group_by(subreddit) |>
+  arrange(desc(tf_idf)) |>
+  # slices the top 10 from each subreddit
+  slice(1:10) 
+
+top_bigram_tfidf <- bigram_tfidf |>
   # arrange in descending order to get highest tf-dfs 
   group_by(subreddit) |>
   arrange(desc(tf_idf)) |>
@@ -120,7 +137,7 @@ subr_top10_tfidf <- subr_tfidf |>
   slice(1:10) 
 
 # visualize
-subr_top10_tfidf |>
+plot_word_tfidf <- top_word_tfidf |>
   ggplot(aes(x = reorder_within(word, tf_idf, subreddit), y = tf_idf, fill = tf_idf)) +
   geom_col() +
   coord_flip() +
@@ -130,6 +147,18 @@ subr_top10_tfidf |>
   labs(x = NULL, 
        y = "TF-IDF",
        title = "Top 10 words by Tf-Idf for Each Subreddit") +
+  scale_fill_viridis('magma')
+
+plot_bigram_tfidf <- top_bigram_tfidf |>
+  ggplot(aes(x = reorder_within(bigram, tf_idf, subreddit), y = tf_idf, fill = tf_idf)) +
+  geom_col() +
+  coord_flip() +
+  theme(legend.position = "none") +
+  facet_wrap(~subreddit, ncol = 2, scales = "free") +
+  scale_x_reordered() +
+  labs(x = NULL, 
+       y = "TF-IDF",
+       title = "Top 10 Bigrams by Tf-Idf for Each Subreddit") +
   scale_fill_viridis('magma')
 
 # ===============================================================================
