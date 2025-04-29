1928eada6c8daa5db25a82475f1a8efb0f66dd7e
Kinglee <ktham27@amherst.edu>
2025-04-29 02:29:24 -0400

    adding admissions keyword data set

--- our-work/kinglee-scratch/kinglee-scratch 2.R
+++ our-work/kinglee-scratch/kinglee-scratch 2.R
@@ -248,6 +248,42 @@ subreddit_sentiment <- sentiment_posts |>
 # save total sentiments
 save(subreddit_sentiment, file='./data/subreddit_sents.Rdata')
 
+# Calculating college-application related posts
+
+# define a vector of application related keywords
+
+application_keywords <- c(
+  "waitlist",  "waitlisted", "waitlisting",
+  "accept",    "accepted",   "acceptance",
+  "apply",     "applied",    "applying", 
+  "application", "admission", "admissions",
+  "defer",     "deferred",   "deferral",
+  "reject",    "rejected",   "rejection",
+  "enroll",    "enrolled",   "enrollment",  
+  "matriculate", "matriculated", "stats"
+)
+
+# calculate # of keyword occurrences for each subreddit
+keyword_counts <- all_posts |>
+  mutate(post_id = row_number()) |> # dummy counter so we can join later
+  # get all tokens from the content
+  unnest_tokens(word, content) |> # one row per word
+  filter(word %in% application_keywords) |> # keep only admission terms
+  group_by(post_id) |>            
+  summarise(keyword_count = n())
+
+# adding dummy counter to original data set
+all_posts <- all_posts |>
+  mutate(post_id = row_number())
+
+# joining counts back to orignal data set
+keyword_posts <- keyword_counts |>
+  right_join(all_posts, by = "post_id") |>
+  # fill all posts with no keywords with 0
+  mutate(keyword_count = replace_na(keyword_count, 0)) |>
+  select(content, date_utc, comments, subreddit,
+         year, month_name, month_num,
+         keyword_count)
 
 # ===============================================================================
 # OVER TIME ANALYSIS
